92d82d6be92b63660c8791b8017fbe1466e20fb3
sufailaqmar <muhammedsufailaqmar36@gmail.com>
2021-10-22 19:05:44 +0530

    part 22/10/2021

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
+window.onscroll = function() {stickyFunction()};
+
+function stickyFunction() {
+  
+  var navbar = document.getElementById("headr");
+  
+  var sticky = navbar.offsetTop;
+  
+  if (window.pageYOffset > sticky) {
+    
+    console.log("world")
+    navbar.classList.add("sticky");
+  } 
+  else {
+    console.log("hello")
+    navbar.classList.remove("sticky");
+  }
+}
+
+
 
 function crossFunc(x) {
   x.classList.toggle("change");
