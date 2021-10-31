062c0f4264eb5c36f7cae229c0eddcd2cd5dcdda
msaqmar <aqmar12396@gmail.com>
2021-10-31 16:30:24 +0530

    31/10/2021 4.30pm

--- js/app.js
+++ js/app.js
@@ -9,7 +9,15 @@ let tl = gsap.timeline({
     },
 });
 
+let tl5 = gsap.timeline({
+    scrollTrigger:{
+        trigger: 'body',
+        start: '0%',
+        end: '50%',
+        scrub: 1,
 
+    },
+});
 
 let tl3 = gsap.timeline({
     scrollTrigger:{
@@ -35,6 +43,8 @@ let tl4 = gsap.timeline({
 
 
 
+
+
 tl.fromTo('#sidelogo', {x: -100, opacity: 0}, {x: 0, opacity: '100%'})
 
 tl.fromTo('#main-img', {x: -100, opacity: 0}, {x: 0, opacity: '100%'})
@@ -49,7 +59,8 @@ tl3.fromTo('#about-img', {y: 100, opacity: 0}, {y: 0, opacity: '100%'})
 tl3.fromTo('#about-h1', {x: -100, opacity: 0}, {x: 0, opacity: '100%'})
 tl3.fromTo('#about-p', {x: -100, opacity: 0}, {x: 0, opacity: '100%'})
 tl3.fromTo('#main-page-btn2', {y: 100, opacity: 0}, {y: 0, opacity: '100%'})
-tl3.fromTo('.back-to-top', {x: 100, opacity: 0}, {x:0, opacity: '100%'})
+
+
 
 
 
@@ -72,7 +83,7 @@ tl4.fromTo('#products-img-5',{x: -100, opacity: 0}, {x:0, opacity: '100%'})
 tl4.fromTo('#products-text-5',{x: -100, opacity: 0}, {x:0, opacity: '100%'})
 
 
-
+tl5.fromTo('.back-to-top', {x: 100, opacity: 0}, {x:0, opacity: '100%'})
 
 
 
